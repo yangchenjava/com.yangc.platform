4cdd73d0292788b812b86f8ae19cec53e1cd2393
yangchenjava <yangchen_java@126.com>
2015-07-15 23:17:12 +0800

    download

--- src/main/java/com/yangc/system/resource/PingResource.java
+++ src/main/java/com/yangc/system/resource/PingResource.java
@@ -1,10 +1,18 @@
 package com.yangc.system.resource;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.log4j.Logger;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +20,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yangc.bean.ResultBean;
 import com.yangc.exception.WebApplicationException;
+import com.yangc.utils.Constants;
+import com.yangc.utils.net.AttachmentUtils;
 
 @Controller
 @RequestMapping("/ping")
@@ -46,6 +56,34 @@ public class PingResource {
 		} catch (Exception e) {
 			e.printStackTrace();
 			return WebApplicationException.build();
+		}
+	}
+
+	@RequestMapping(value = "download", method = RequestMethod.GET)
+	public void download(HttpServletRequest request, HttpServletResponse response) {
+		logger.info("download");
+		String path = new File(request.getSession().getServletContext().getRealPath("/")).getParent() + Constants.PORTRAIT_PATH;
+		File file = new File(path, "113_1414850458106_original.png");
+
+		response.reset();
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+		AttachmentUtils.convertAttachmentFileName(request, response, "你好 我是杨晨.png");
+		response.setHeader("Content-Length", "" + file.length());
+		OutputStream out = null;
+		try {
+			out = response.getOutputStream();
+			out.write(FileUtils.readFileToByteArray(file));
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (out != null) out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
