1711121e45ea9366813adaaecb971c4e147891f6
yangchenjava <yangchen_java@126.com>
2016-03-23 22:10:19 +0800

    com.yangc.platform

--- src/main/java/com/yangc/bean/SessionFactoryBean.java
+++ src/main/java/com/yangc/bean/SessionFactoryBean.java
@@ -1,9 +1,14 @@
 package com.yangc.bean;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate3.LocalSessionFactoryBean;
 
@@ -20,6 +25,38 @@ public class SessionFactoryBean extends LocalSessionFactoryBean {
 				if (resource.exists()) resources.add(resource);
 			}
 			super.setMappingDirectoryLocations(resources.toArray(new Resource[0]));
+		}
+	}
+
+	@Override
+	public void setMappingJarLocations(Resource... mappingJarLocations) {
+		if (mappingJarLocations != null) {
+			List<Resource> resources = new ArrayList<Resource>();
+			for (Resource mappingJarLocation : mappingJarLocations) {
+				JarFile jarFile = null;
+				try {
+					jarFile = new JarFile(mappingJarLocation.getFile());
+					Enumeration<JarEntry> jarEntries = jarFile.entries();
+					while (jarEntries.hasMoreElements()) {
+						JarEntry jarEntry = jarEntries.nextElement();
+						if (jarEntry.getName().endsWith(".hbm.xml") && jarEntry.getName().contains(Constants.DB_NAME)) {
+							InputStreamResource resource = new InputStreamResource(jarFile.getInputStream(jarEntry));
+							if (resource.exists()) resources.add(resource);
+						}
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					try {
+						if (jarFile != null) {
+							jarFile.close();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			super.setMappingLocations(resources.toArray(new Resource[0]));
 		}
 	}
 
